aa75edf5c0306a9dd560d530b5d4eb5890573a31
Ismail Bourbie <bourbieismail@gmail.com>
2018-09-05 16:42:07 +0200

    add icon

--- inc/head.php
+++ inc/head.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title><?= SITE_NAME?></title>
+    <link rel="icon" href="images/credit.jpg">
     <link rel="stylesheet" href="css/font-awesome.min.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/main.css">
